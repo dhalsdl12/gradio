990c3fd6a4f5d34f4524b1189b42b1ad8f7818d5
dhalsdl12 <dhalsdl12@gmail.com>
2023-07-12 13:19:27 +0900

    test

---
+++ video.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+from moviepy.editor import VideoFileClip
+ 
+def get_duration(filename):
+    clip = VideoFileClip(filename)
+    return clip.duration
+
+
+print(cv2.__version__)
+
+filepath = './cat.mp4'
+video = cv2.VideoCapture(filepath)
+
+if not video.isOpened():
+    print("Could not Open : ", filepath)
+    exit(0)
+
+length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = video.get(cv2.CAP_PROP_FPS)
+frame = 25
+print("length :", length)
+print("fps :", fps)
+
+
+try:
+    if not os.path.exists(filepath[:-4]):
+        os.makedirs(filepath[:-4])
+except OSError:
+    print ('Error: Creating directory. ' +  filepath[:-4])
+
+count = 0
+
+while(video.isOpened()):
+    if length <= (fps//frame) * (count + 1):
+        break
+    ret, image = video.read()
+    if(int(video.get(1)) % (fps//frame) == 0): #앞서 불러온 fps 값을 사용하여 1초마다 추출
+        cv2.imwrite(filepath[:-4] + "/frame%d.jpg" % count, image)
+        print('Saved frame number :', str(int(video.get(1))))
+        count += 1
+    
+        
+video.release()
